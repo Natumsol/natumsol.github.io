6ab85abda7835f8c72794f1c325148654fd7ef16
Natumsol <Natumsol@users.noreply.github.com>
2021-01-25 03:09:14 +0000

    deployed via Gatsby Publish Action 🎩 for 1a1cd911019ff1a05037876bb1d2828826b4dce8

---
+++ project/2048-react-reudx/precache-manifest.9503952ba1c7cdeb8c695a062556612c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75deb8455bc7f7aad861aedeab785c0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02026439991ad960693b",
+    "url": "/static/css/main.ca84f105.chunk.css"
+  },
+  {
+    "revision": "9afe726f6645f7322518",
+    "url": "/static/js/2.269dd912.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.269dd912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02026439991ad960693b",
+    "url": "/static/js/main.585c87c0.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
